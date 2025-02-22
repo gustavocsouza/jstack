bab541abbaadfc51cd5b1d016f0ce2344b4dffe5
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2025-02-22 15:34:39 -0300

    first commit

---
+++ Módulo 01/1- Módulos no JS/index.js
@@ -0,0 +1,7 @@
+// const { printName, lastName } = require('./printName');
+
+// printName('Gustavo ' + lastName);
+
+const os = require('os');
+
+console.log(os.type());
---
+++ Módulo 01/1- Módulos no JS/printName.js
@@ -0,0 +1,7 @@
+function printName(name) {
+  console.log(name);
+}
+
+const lastName = 'Cardoso';
+
+module.exports = { printName, lastName};
