0158d162faa02cc24b7bc08ce1c9c3ac7dcff098
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2025-02-22 20:56:10 -0300

    criando API com HTTP nativo

--- Módulo 01/2- Primeira API/firstapi/src/index.js
+++ Módulo 01/2- Primeira API/firstapi/src/index.js
@@ -1,8 +1,44 @@
 const http = require('http');
+const { URL } = require('url');
+
+const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'content-type': 'text/html' });
-  response.end('<h1>Hello world! </h1>');
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)  
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`
+    id = splitEndpoint[1];
+  }
+
+  const route = routes.find((routeObj) => 
+    (routeObj.endpoint === pathname && routeObj.method === request.method)
+  );
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id }
+
+    route.handler(request, response);
+  } else {
+    response.writeHead(404, { 'content-type': 'text/html' });
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+  }
+
+
+  // if (request.url === '/users' && request.method === 'GET') {
+  //   UserController.listUsers(request, response);
+  // } else {
+  //   response.writeHead(404, { 'content-type': 'text/html' });
+  //   response.end(`Cannot ${request.method} ${request.url}`);
+  // }
+
 });
 
-server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000')); 
---
+++ Módulo 01/2- Primeira API/firstapi/src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Gustavo',
+  },
+  {
+    id: 2,
+    name: 'João',
+  },
+  {
+    id: 3,
+    name: 'Maria',
+  },
+  {
+    id: 4,
+    name: 'Pedro',
+  },
+];
