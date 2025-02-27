8bc52701ce98dc77baa11e70554af1c37217227a
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2025-02-27 15:53:00 -0300

    Finalizando API

--- Módulo 02/mycontacts/eslint.config.mjs
+++ Módulo 02/mycontacts/eslint.config.mjs
@@ -4,7 +4,19 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    },
+    rules: {
+      "no-unused-vars": ['error', { argsIgnorePattern: 'next' }]
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
   pluginJs.configs.recommended,
 ];
--- Módulo 02/mycontacts/src/app/controllers/ContactController.js
+++ Módulo 02/mycontacts/src/app/controllers/ContactController.js
@@ -3,7 +3,9 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 class ContactController {
   async index(request, response) {
     // Listar todos os registros
-    const contacts = await ContactsRepository.findAll();
+    const { orderBy } = request.query;
+
+    const contacts = await ContactsRepository.findAll(orderBy);
 
     response.json(contacts);
   }
@@ -21,23 +23,64 @@ class ContactController {
     response.json(contact);
   }
 
-  store() {
-    // Criar novo registro
+  async store(request, response) {
+    // Cadastrar um novo registro
+    const {
+      name, email, phone, category_id
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id
+    });
+
+    response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id
+    } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: "Contact not found" });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id
+    });
+
+    response.json(contact);
+
   }
 
   async delete(request, response) {
     // Deletar um registro
     const { id } = request.params;
-
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      return response.status(404).json({ error: 'Contact not found' });
-    }
 
     await ContactsRepository.delete(id);
 
